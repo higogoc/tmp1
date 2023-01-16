025c7f7a90fe8b55dc54017119aec869e84a154f
higogoc <selee203@naver.com>
2023-01-16 21:39:56 +0900

    tmp

--- capture_xr/main.js
+++ capture_xr/main.js
@@ -1,9 +1,10 @@
 import * as THREE from '../../libs/three.js-r132/build/three.module.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+      // 캡쳐 버튼 추가 
+  const captureButton = document.querySelector("#capture-button");
   const initialize = async() => {
     const arButton = document.querySelector("#ar-button");
-
     // check and request webxr session 
     const supported = navigator.xr && await navigator.xr.isSessionSupported('immersive-ar');
     if (!supported) {
@@ -16,8 +17,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera();
     const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
+    // Set the size of the renderer
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
+    // Add the renderer to the HTML body
     document.body.appendChild(renderer.domElement);
 
     // create AR object
@@ -48,7 +51,17 @@ document.addEventListener('DOMContentLoaded', () => {
       arButton.textContent = "End";
 
       renderer.setAnimationLoop(() => {
-	renderer.render(scene, camera);
+    // Render the scene
+    renderer.render(scene, camera);
+    // 버튼 클릭 시 실행될 핸들러 함수 추가
+    captureButton.addEventListener("click", captureHandler);
+    // Capture the image
+      var imageData = renderer.domElement.toDataURL();
+      // Create an image element and set its source to the captured image
+      var img = document.createElement('img');
+      img.src = imageData;
+      // Add the image to the HTML body
+      document.body.appendChild(img);
       });
     }
     const end = async() => {
@@ -64,6 +77,14 @@ document.addEventListener('DOMContentLoaded', () => {
 	start();
       }
     });
+
+    function captureHandler() {
+      
+      var imageData = renderer.domElement.toDataURL();
+      var img = document.createElement('img');
+      img.src = imageData;
+      document.body.appendChild(img);
+    }
 
   }
 
